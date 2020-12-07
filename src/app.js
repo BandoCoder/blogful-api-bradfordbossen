1a9246e212a7bd772b23850d22d5987722e88c30
kittybagpipes <sparticus978@gmail.com>
2020-12-07 11:42:58 -0800

    tested

--- src/app.js
+++ src/app.js
@@ -15,7 +15,27 @@ app.use(helmet());
 app.use(cors());
 
 app.get("/articles", (req, res, next) => {
-  res.send("All Articles");
+  const knexInstance = req.app.get("db");
+
+  ArticlesService.getAllArticles(knexInstance)
+    .then((articles) => {
+      res.json(articles);
+    })
+    .catch(next);
+});
+
+app.get(`/articles/:article_id`, (req, res, next) => {
+  const knexInstance = req.app.get("db");
+  ArticlesService.getById(knexInstance, req.params.article_id)
+    .then((article) => {
+      if (!article) {
+        return res.status(404).json({
+          error: { message: `Article doesn't exist` },
+        });
+      }
+      res.json(article);
+    })
+    .catch(next);
 });
 
 app.get("/", (req, res) => {
